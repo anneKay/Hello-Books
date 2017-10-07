2210db8464f76d3a5729f75b3bb5263fe9ba4d5a
Annekay <nwannauk@gmail.com>
2017-10-07 21:30:54 +0100

    updates the client folder

---
+++ client/main.js
@@ -0,0 +1,11 @@
+
+var modalSignUp = document.getElementById('sign_up');
+var modalLogIn = document.getElementById("log_in")
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modalSignUp || event.target == modalLogIn) {
+        modalSignUp.style.display = "none";
+        modalLogIn.style.display = "none";
+    }
+}
